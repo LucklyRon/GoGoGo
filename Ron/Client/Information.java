5ac19f7c19f35028591b64ea0dbf190378255361
LucklyRon <18734879039@163.com>
2017-03-30 21:20:46 +0800

    Ron

---
+++ Ron/Client/Information.java
@@ -0,0 +1,70 @@
+package com.example.entry;
+
+import java.io.Serializable;
+
+public class Information implements Serializable {
+	private static final long serialVersionUID = -7620435178023928252L;
+	public String name;
+	public String Age;
+	public String Sex;
+	public String Day;
+	public String image;
+	public String Id;
+	public String Description;
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return Age;
+	}
+
+	public void setAge(String age) {
+		Age = age;
+	}
+
+	public String getSex() {
+		return Sex;
+	}
+
+	public void setSex(String sex) {
+		Sex = sex;
+	}
+
+	public String getDay() {
+		return Day;
+	}
+
+	public void setDay(String day) {
+		Day = day;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+}
---
+++ Ron/Client/PublishActivity.java
@@ -0,0 +1,190 @@
+package com.example.demo.activity;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
+
+import org.apache.http.entity.StringEntity;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.demo.R;
+import com.example.entry.Information;
+import com.example.utils.UploadUtil;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.RequestParams;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
+
+public class PublishActivity extends Activity implements OnClickListener {
+	private Information info;
+	private TextView txtName;
+	private TextView txtAge;
+	private TextView txtSex;
+	private TextView txtDay;
+	private TextView txtDescription;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.publish_activity);
+		Button btnBack = (Button) this.findViewById(R.id.back);
+		Button btnPublish = (Button) this.findViewById(R.id.ok);
+		btnBack.setOnClickListener(this);
+		btnPublish.setOnClickListener(this);
+		txtAge = (TextView) findViewById(R.id.age);
+		txtDay = (TextView) findViewById(R.id.day);
+		txtSex = (TextView) findViewById(R.id.sex);
+		txtDescription = (TextView) findViewById(R.id.description);
+		txtName = (TextView) findViewById(R.id.name);
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		switch (arg0.getId()) {
+		case R.id.back:
+			startActivity(new Intent(PublishActivity.this, MainActivity.class));
+			this.finish();
+			break;
+		case R.id.ok:
+			String Id = UUID.randomUUID().toString();
+			info = GetInfo(Id);
+			PublishInfo(info, Id);
+
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	private Information GetInfo(String id) {
+		Information information = new Information();
+		information.setAge(txtAge.getText().toString());
+		information.setDay(txtDay.getText().toString());
+		information.setId(id);
+		information.setName(txtName.getText().toString());
+		information.setSex(txtSex.getText().toString());
+		information.setDescription(txtDescription.getText().toString());
+		return information;
+	}
+
+	private void PublishInfo(Information information, String id) {
+		PublishData(information);
+		Resources r = this.getApplicationContext().getResources();
+		Bitmap bitmap = BitmapFactory.decodeResource(r, R.drawable.ic_launcher);
+		UploadUtil.uploadFile(saveFile(bitmap, id + ".jpeg"),
+				"http://172.20.16.84:8080/Demo/UploadFileServlet");
+	}
+
+	private void PublishData(Information information) {
+		try {
+			RequestParams params = new RequestParams();
+			// 封装一个JSON对象
+			JSONObject param = new JSONObject();
+			param.put("Id", information.getId());
+			param.put("name", information.getName());
+			param.put("age", information.getAge());
+			param.put("sex", information.getSex());
+			param.put("day", information.getDay());
+			param.put("description", information.getDescription());
+			StringEntity sEntity = new StringEntity(param.toString(), "utf-8");
+			params.setBodyEntity(sEntity);
+			Log.e("Test", param.toString());
+			HttpUtils http = new HttpUtils();
+			http.configCurrentHttpCacheExpiry(1000 * 10);
+			http.send(HttpMethod.POST,
+					"http://172.20.16.84:8080/Demo/AddDataServerlet", params,
+					new RequestCallBack<String>() {
+						ProgressDialog waitingDialog = new ProgressDialog(
+								PublishActivity.this);
+
+						@Override
+						public void onSuccess(ResponseInfo<String> responseInfo) {
+							waitingDialog.dismiss();
+							Log.e("Test", "dialog");
+							startActivity(new Intent(PublishActivity.this,
+									MainActivity.class));
+							PublishActivity.this.finish();
+						}
+
+						@Override
+						public void onStart() {
+							waitingDialog.setTitle("Wait for upload");
+							waitingDialog.setMessage("upload information...");
+							waitingDialog.setIndeterminate(true);
+							waitingDialog.setCancelable(false);
+							waitingDialog.show();
+						}
+
+						@Override
+						public void onFailure(HttpException error, String msg) {
+							Toast.makeText(getApplicationContext(),
+									"upload failed", Toast.LENGTH_LONG).show();
+							Log.e("error", msg);
+							Log.e("error", error.toString());
+						}
+					});
+
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public File saveFile(Bitmap bm, String fileName) {
+		String path = getSDPath() + "/revoeye/";
+		File dirFile = new File(path);
+		if (!dirFile.exists()) {
+			dirFile.mkdir();
+		}
+		File myCaptureFile = null;
+		try {
+			myCaptureFile = new File(path + fileName);
+			BufferedOutputStream bos = new BufferedOutputStream(
+					new FileOutputStream(myCaptureFile));
+			bm.compress(Bitmap.CompressFormat.JPEG, 80, bos);
+			bos.flush();
+			bos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return myCaptureFile;
+	}
+
+	public String getSDPath() {
+		File sdDir = null;
+		boolean sdCardExist = Environment.getExternalStorageState().equals(
+				Environment.MEDIA_MOUNTED); // 判断sd卡是否存在
+		if (sdCardExist) {
+			sdDir = Environment.getExternalStorageDirectory();// 获取跟目录
+		} else {
+			return "/data/data/com.example.demo/cache";
+		}
+		return sdDir.toString();
+	}
+}
